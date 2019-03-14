bb2c38b3d65b691772a5f26e51c48488da362c00
robertogamez <vn00m8demail.wal/mart.com>
2019-03-14 16:53:11 -0600

    Trbjando con mas operaciones LINQ

--- MoreEffectiveLINQ/Program.cs
+++ MoreEffectiveLINQ/Program.cs
@@ -33,21 +33,47 @@ namespace MoreEffectiveLINQ
             });
         }
 
-        static void CheckOrdersForRefunds()
-        {
-            List<Order> orders = new List<Order>()
+        static List<Order> orders = new List<Order>()
             {
                 new Order{ Id = 123, Amount = 29.95m, CustomerName = "Mark", Status = "Delivered" },
                 new Order{ Id = 456, Amount = 45.00m, CustomerName = "Mark", Status = "Refunded" },
                 new Order{ Id = 768, Amount = 32.50m, CustomerName = "Mark", Status = "Delivered" }
             };
 
+        static void CheckOrdersForRefunds()
+        {
             bool anyRefunded = orders.Any(o => o.Status == "Refunded");
             bool allDelivered = orders.All(o => o.Status == "Delivered");
 
             Console.WriteLine("Any: {0}, Delivered: {1}", anyRefunded, allDelivered);
         }
 
+        static void CountRefundedOrders()
+        {
+            Console.WriteLine("Refunded count: " + orders.Count(o => o.Status == "Refunded"));
+            Console.ReadLine();
+        }
+
+        static void GetOrderTotal()
+        {
+            Console.WriteLine("Count: " + orders.Sum(o => o.Amount));
+            Console.ReadLine();
+        }
+
+        static void GroupByCustomer()
+        {
+            orders.GroupBy(o => o.CustomerName).ToDictionary(g => g.Key, g => g.ToList());
+        }
+
+        static void Challengue()
+        {
+            "10, 5, 0, 8, 10, 1, 4, 0, 10, 1"
+                .Split(',')
+                .Select(int.Parse)
+                .OrderBy(n => n)
+                .Skip(3)
+                .Sum();
+        }
 
         static void Main(string[] args)
         {
@@ -64,8 +90,8 @@ namespace MoreEffectiveLINQ
             //foreach (var customer in customers.Where(c => !string.IsNullOrEmpty(c.Email)))
             //{
             //    Console.WriteLine("Sending email to {0}", customer.Name);
-            //}
-            GetListOfFileSizes();
+            CountRefundedOrders();
+            GetOrderTotal();
 
             Console.ReadLine();
         }
