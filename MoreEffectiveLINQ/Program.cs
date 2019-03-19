fa565e9310ed0aca7d050276b6ac9cfcf3393be8
robertogamez <vn00m8demail.wal/mart.com>
2019-03-19 16:52:05 -0600

    Trabajando con pipelines

--- MoreEffectiveLINQ/Program.cs
+++ MoreEffectiveLINQ/Program.cs
@@ -89,8 +89,8 @@ namespace MoreEffectiveLINQ
         {
             var result = "6,1-3,2-4"
                 .Split(',')
-                .Select(x => x.Split('-'));
-                .Select(p => new { First = int.Parse(p[0]), Last = int.Parse(p.Last()) });
+                .Select(x => x.Split('-'))
+                .Select(p => new { First = int.Parse(p[0]), Last = int.Parse(p.Last()) })
                 .SelectMany(r => Enumerable.Range(r.First, r.Last - r.First + 1))
                 .OrderBy(r => r)
                 .Distinct();
